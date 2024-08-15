2378ef6d1e95ee0475472b8567a0965b7b367d33
Aris Ripandi <riipandi@gmail.com>
2024-08-15 19:55:53 +0700

    update solid-start-playground

--- solid-start-playground/src/api/db.ts
+++ solid-start-playground/src/api/db.ts
@@ -1,5 +1,6 @@
-import { drizzle, BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
+import type { BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
+import { drizzle } from "drizzle-orm/better-sqlite3";
 
 const sqlite = new Database("./drizzle/db.sqlite");
 
